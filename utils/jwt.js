73876daa5efb92de482a6ae8b443f11738f297db
lucas Garcia <lucastobiasg@gmail.com>
2026-06-11 13:03:43 -0500

    fully working backend-all tests passing

--- utils/jwt.js
+++ utils/jwt.js
@@ -7,7 +7,6 @@ export function createToken(payload) {
   return jwt.sign(payload, SECRET, { expiresIn: "7d" });
 }
 
-/** Extracts the payload from a token */
 export function verifyToken(token) {
   return jwt.verify(token, SECRET);
 }
